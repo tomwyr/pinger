cc4947a33b08f878900f664bfd3515ce33c589b3
tomwyr <tomwyr@hotmail.com>
2020-01-28 23:31:19 +0100

    Show different home content depending on data

--- lib/page/home_page.dart
+++ lib/page/home_page.dart
@@ -1,9 +1,15 @@
+import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:pinger/extensions.dart';
 import 'package:pinger/page/archive_page.dart';
 import 'package:pinger/page/intro_page.dart';
+import 'package:pinger/page/ping_page.dart';
 import 'package:pinger/page/search_page.dart';
 import 'package:pinger/page/settings_page.dart';
+import 'package:pinger/store/favorites_store.dart';
+import 'package:pinger/store/history_store.dart';
+import 'package:pinger/store/ping_store.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -11,13 +17,34 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final _historyStore = HistoryStore();
+  final _favoritesStore = FavoritesStore();
+  final _pingStore = PingStore();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: _buildAppBar(),
       body: Padding(
         padding: EdgeInsets.all(32.0),
-        child: _buildNoSuggestions(),
+        child: Observer(builder: (_) {
+          final history = _historyStore.items;
+          final favorites = _favoritesStore.items;
+          final ping = _pingStore.value;
+          if (history.isEmpty && favorites.isEmpty && ping == null) {
+            return _buildNoSuggestions();
+          } else {
+            return Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                _buildSearchBar(),
+                if (ping != null) ..._buildCurrentPing(ping),
+                ..._buildFavorites(favorites),
+                ..._buildHistory(history),
+              ],
+            );
+          }
+        }),
       ),
     );
   }
@@ -68,6 +95,121 @@ class _HomePageState extends State<HomePage> {
           style: TextStyle(fontSize: 14.0),
         ),
       ],
+    );
+  }
+
+  List<Widget> _buildCurrentPing(Ping ping) {
+    return [
+      _buildSectionTitle('Current'),
+      Row(children: [
+        Image.network(
+          'https://www.netflix.com/favicon.ico',
+          width: 24.0,
+          height: 24.0,
+        ),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 12.0),
+            child: Text(ping.host),
+          ),
+        ),
+        GestureDetector(
+          onTap: () => push(PingPage()),
+          child: Text(
+            'Go back',
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+        ),
+      ]),
+    ];
+  }
+
+  List<Widget> _buildFavorites(List<FavoriteItem> favorites) {
+    return [
+      _buildSectionTitle(
+        'Favorites',
+        icon: favorites.isNotEmpty ? Icons.compare_arrows : null,
+      ),
+      if (favorites.isEmpty)
+        DottedBorder(
+          padding: EdgeInsets.all(16.0),
+          child: Text(
+            'Bookmark pinged hosts to access them easily',
+            textAlign: TextAlign.center,
+            style: TextStyle(color: Colors.grey),
+          ),
+        )
+      else
+        ListView(
+          shrinkWrap: true,
+          children: favorites
+              .map((it) => Padding(
+                    padding: EdgeInsets.symmetric(vertical: 4.0),
+                    child: Row(children: <Widget>[
+                      Expanded(child: Text(it.host)),
+                      Text('${it.pingCount}'),
+                    ]),
+                  ))
+              .toList(),
+        ),
+    ];
+  }
+
+  List<Widget> _buildHistory(List<HistoryItem> history) {
+    return [
+      _buildSectionTitle(
+        'Recent',
+        icon: history.isNotEmpty ? Icons.history : null,
+      ),
+      if (history.isEmpty)
+        DottedBorder(
+          padding: EdgeInsets.all(16.0),
+          child: Text(
+            'Latest host will show up here after you ping one',
+            textAlign: TextAlign.center,
+            style: TextStyle(color: Colors.grey),
+          ),
+        )
+      else
+        ListView(
+          shrinkWrap: true,
+          children: history
+              .map((it) => Padding(
+                    padding: EdgeInsets.symmetric(vertical: 4.0),
+                    child: Row(children: [
+                      Expanded(child: Text(it.host)),
+                      Text(_getTimestampLabel(it.timestamp)),
+                    ]),
+                  ))
+              .toList(),
+        ),
+    ];
+  }
+
+  String _getTimestampLabel(DateTime timestamp) {
+    final diff = DateTime.now().difference(timestamp);
+    if (diff.inDays >= 7) return '${diff.inDays ~/ 7} w';
+    if (diff.inHours >= 24) return '${diff.inHours ~/ 24} d';
+    if (diff.inMinutes >= 60) return '${diff.inMinutes ~/ 60} h';
+    if (diff.inSeconds >= 60) return '${diff.inSeconds ~/ 60} m';
+    return '${diff.inSeconds} s';
+  }
+
+  Widget _buildSectionTitle(String title, {IconData icon}) {
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 16.0),
+      child: Row(children: <Widget>[
+        Expanded(
+          child: Text(
+            title,
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 18.0,
+            ),
+          ),
+        ),
+        if (icon != null) Icon(icon)
+      ]),
     );
   }
 
