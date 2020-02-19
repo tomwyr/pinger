e66d9d6efa591e1a3f1c5fbc433c53e26606fa6a
tomwyr <tomwyr@hotmail.com>
2020-02-19 20:12:52 +0100

    Add prefs get and save method for ping sessions

--- lib/service/pinger_prefs.dart
+++ lib/service/pinger_prefs.dart
@@ -1,12 +1,14 @@
 import 'dart:convert';
 
 import 'package:injectable/injectable.dart';
+import 'package:pinger/model/ping_session.dart';
 import 'package:pinger/model/user_settings.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 @singleton
 class PingerPrefs {
   final String _userSettingsKey = 'userSettings';
+  final String _archiveSessionsKey = 'archiveSessions';
 
   final SharedPreferences _sharedPrefs;
 
@@ -23,5 +25,23 @@ class PingerPrefs {
   Future<void> saveUserSettings(UserSettings settings) async {
     final jsonString = jsonEncode(settings.toJson());
     await _sharedPrefs.setString(_userSettingsKey, jsonString);
+  }
+
+  List<PingSession> getArchiveSessions() {
+    if (_sharedPrefs.containsKey(_archiveSessionsKey)) {
+      return _sharedPrefs
+          .getStringList(_archiveSessionsKey)
+          .map(jsonDecode)
+          .map((it) => PingSession.fromJson(it as Map<String, dynamic>))
+          .toList();
+    }
+    return null;
+  }
+
+  Future<void> saveArchiveSession(PingSession session) async {
+    final allSessions = (getArchiveSessions() ?? [])..add(session);
+    final jsonStringList =
+        allSessions.map((it) => it.toJson()).map(jsonEncode).toList();
+    await _sharedPrefs.setStringList(_archiveSessionsKey, jsonStringList);
   }
 }
