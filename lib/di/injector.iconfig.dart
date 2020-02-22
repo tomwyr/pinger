b5483164ff1d79560e1a30b399d6bdaf43b57d55
tomwyr <tomwyr@hotmail.com>
2020-02-22 21:42:01 +0100

    Commit generated code

--- lib/di/injector.iconfig.dart
+++ lib/di/injector.iconfig.dart
@@ -6,12 +6,12 @@
 
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:pinger/service/pinger_prefs.dart';
-import 'package:pinger/store/favorites_store.dart';
-import 'package:pinger/store/ping_store.dart';
-import 'package:pinger/store/history_store.dart';
-import 'package:pinger/store/settings_store.dart';
-import 'package:pinger/store/hosts_store.dart';
 import 'package:pinger/store/archive_store.dart';
+import 'package:pinger/store/favorites_store.dart';
+import 'package:pinger/store/history_store.dart';
+import 'package:pinger/store/hosts_store.dart';
+import 'package:pinger/store/ping_store.dart';
+import 'package:pinger/store/settings_store.dart';
 import 'package:get_it/get_it.dart';
 
 Future<void> $initGetIt(GetIt g, {String environment}) async {
@@ -25,14 +25,14 @@ void _registerEagerSingletons(GetIt g, String environment) {
   g.registerSingleton<PingerPrefs>(PingerPrefs(
     g<SharedPreferences>(),
   ));
-  g.registerSingleton<FavoritesStore>(FavoritesStore());
-  g.registerSingleton<PingStore>(PingStore());
-  g.registerSingleton<HistoryStore>(HistoryStore());
-  g.registerSingleton<SettingsStore>(SettingsStore(
+  g.registerSingleton<ArchiveStore>(ArchiveStore(
     g<PingerPrefs>(),
   ));
+  g.registerSingleton<FavoritesStore>(FavoritesStore());
+  g.registerSingleton<HistoryStore>(HistoryStore());
   g.registerSingleton<HostsStore>(HostsStore());
-  g.registerSingleton<ArchiveStore>(ArchiveStore(
+  g.registerSingleton<PingStore>(PingStore());
+  g.registerSingleton<SettingsStore>(SettingsStore(
     g<PingerPrefs>(),
   ));
 }
