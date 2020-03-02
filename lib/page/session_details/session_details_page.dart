b940fb55214a14eebb167a8b9b714e56d45526c4
tomwyr <tomwyr@hotmail.com>
2020-03-02 07:06:46 +0100

    Fix slivers scrolling issues

--- lib/page/session_details/session_details_page.dart
+++ lib/page/session_details/session_details_page.dart
@@ -8,7 +8,7 @@ import 'package:pinger/page/session_details/session_details_tabs/session_details
 import 'package:pinger/page/session_details/session_details_tabs/session_details_more.dart';
 import 'package:pinger/page/session_details/session_details_tabs/session_details_results.dart';
 import 'package:pinger/store/archive_store.dart';
-import 'package:pinger/widgets/const_extent_header_delegate.dart';
+import 'package:pinger/widgets/snapping_app_bar_with_tabs.dart';
 
 enum SessionDetailsTab { results, global, info, more }
 
@@ -23,9 +23,11 @@ class SessionDetailsPage extends StatefulWidget {
 
 class _SessionDetailsPageState extends State<SessionDetailsPage>
     with SingleTickerProviderStateMixin {
+  static const _collapsingTileHeight = 480.0;
+  static const _collapsedHeight = kToolbarHeight + 48.0;
+  static const _collapsedOffset = _collapsingTileHeight - _collapsedHeight;
+
   final ArchiveStore _archiveStore = Injector.resolve();
-  final double _collapsingTileHeight = 432.0;
-  final double _tabBarHeight = 48.0;
 
   SessionDetailsTab _selectedTab = SessionDetailsTab.results;
   ScrollController _scrollController;
@@ -52,62 +54,51 @@ class _SessionDetailsPageState extends State<SessionDetailsPage>
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: NestedScrollView(
-          controller: _scrollController,
-          headerSliverBuilder: (_, __) => <Widget>[
-            SliverAppBar(
-              pinned: true,
-              elevation: 0.0,
-              expandedHeight: _collapsingTileHeight,
-              backgroundColor: Theme.of(context).canvasColor,
-              leading: BackButton(color: Colors.black),
-              actions: <Widget>[
-                IconButton(
-                  icon: Icon(Icons.delete, color: Colors.black),
-                  onPressed: () async {
-                    await _archiveStore.deleteSession(widget.session.id);
-                    pop();
-                  },
-                ),
+        child: SnappingAppBarWithTabs(
+          collapsedOffset: _collapsedOffset,
+          scrollController: _scrollController,
+          scroller: _scrollLayoutTo,
+          appBar: SliverAppBar(
+            pinned: true,
+            elevation: 2.0,
+            forceElevated: true,
+            expandedHeight: _collapsingTileHeight,
+            backgroundColor: Theme.of(context).canvasColor,
+            leading: BackButton(color: Colors.black),
+            actions: <Widget>[
+              IconButton(
+                icon: Icon(Icons.delete, color: Colors.black),
+                onPressed: () async {
+                  await _archiveStore.deleteSession(widget.session.id);
+                  pop();
+                },
+              ),
+            ],
+            flexibleSpace: LayoutBuilder(
+              builder: (_, constraints) => SessionSummaryCollapsingTile(
+                expansion: (constraints.maxHeight - _collapsedHeight) /
+                    (_collapsingTileHeight - _collapsedHeight),
+                session: widget.session,
+                minExtent: kToolbarHeight,
+                maxExtent: _collapsingTileHeight,
+              ),
+            ),
+            bottom: TabBar(
+              onTap: _onTabTap,
+              controller: _tabController,
+              labelColor: Colors.black,
+              indicatorColor: Colors.lightBlue,
+              tabs: [
+                Tab(text: "Results"),
+                Tab(text: "Global"),
+                Tab(text: "Info"),
+                Tab(text: "More"),
               ],
-              flexibleSpace: LayoutBuilder(
-                builder: (_, constraints) => SessionSummaryCollapsingTile(
-                  expansion: (constraints.maxHeight - kToolbarHeight) /
-                      (_collapsingTileHeight - kToolbarHeight),
-                  session: widget.session,
-                  minExtent: kToolbarHeight,
-                  maxExtent: _collapsingTileHeight,
-                ),
-              ),
             ),
-            SliverPersistentHeader(
-              pinned: true,
-              delegate: ConstExtentHeaderDelegate(
-                extent: _tabBarHeight,
-                builder: (_) => Material(
-                  elevation: 4.0,
-                  child: Container(
-                    color: Theme.of(context).canvasColor,
-                    child: TabBar(
-                      onTap: _onTabTap,
-                      controller: _tabController,
-                      labelColor: Colors.black,
-                      indicatorColor: Colors.lightBlue,
-                      tabs: [
-                        Tab(text: "Results"),
-                        Tab(text: "Global"),
-                        Tab(text: "Info"),
-                        Tab(text: "More"),
-                      ],
-                    ),
-                  ),
-                ),
-              ),
-            ),
-          ],
-          body: TabBarView(
+          ),
+          tabBarView: TabBarView(
             controller: _tabController,
-            children: <Widget>[
+            children: [
               SessionDetailsResults(),
               SessionDetailsGlobal(),
               SessionDetailsInfo(),
@@ -122,24 +113,23 @@ class _SessionDetailsPageState extends State<SessionDetailsPage>
   void _onTabTap(int index) {
     final tab = SessionDetailsTab.values[index];
     if (_selectedTab == tab) {
-      final offset = _scrollController.offset == _collapsingTileHeight
-          ? 0.0
-          : _collapsingTileHeight;
-      _scrollTo(offset);
+      final offset =
+          _scrollController.offset == _collapsedOffset ? 0.0 : _collapsedOffset;
+      _scrollLayoutTo(offset);
     } else {
       _selectedTab = tab;
       _tabController.animateTo(SessionDetailsTab.values.indexOf(tab));
-      if (_scrollController.offset < _collapsingTileHeight) {
-        _scrollTo(_collapsingTileHeight);
+      if (_scrollController.offset < _collapsedOffset) {
+        _scrollLayoutTo(_collapsedOffset);
       }
     }
   }
 
-  void _scrollTo(double offset) {
-    _scrollController.animateTo(
+  Future<void> _scrollLayoutTo(double offset) async {
+    await _scrollController.animateTo(
       offset,
-      duration: Duration(milliseconds: 300),
-      curve: Curves.linear,
+      duration: Duration(milliseconds: 200),
+      curve: Curves.easeOut,
     );
   }
 }
---
+++ lib/widgets/snapping_app_bar_with_tabs.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+
+class SnappingAppBarWithTabs extends StatefulWidget {
+  final SliverAppBar appBar;
+  final TabBarView tabBarView;
+  final double collapsedOffset;
+  final ScrollController scrollController;
+  final Future<void> Function(double) scroller;
+
+  const SnappingAppBarWithTabs({
+    Key key,
+    @required this.appBar,
+    @required this.tabBarView,
+    @required this.collapsedOffset,
+    @required this.scrollController,
+    @required this.scroller,
+  }) : super(key: key);
+
+  @override
+  _SnappingAppBarWithTabsState createState() => _SnappingAppBarWithTabsState();
+}
+
+class _SnappingAppBarWithTabsState extends State<SnappingAppBarWithTabs> {
+  bool _isSnapping = false;
+  ScrollUpdateNotification _lastScrollUpdate;
+
+  @override
+  Widget build(BuildContext context) {
+    return NotificationListener(
+      onNotification: _onScrollEvent,
+      child: NestedScrollView(
+        controller: widget.scrollController,
+        headerSliverBuilder: (headerContext, __) => <Widget>[
+          SliverOverlapAbsorber(
+            handle:
+                NestedScrollView.sliverOverlapAbsorberHandleFor(headerContext),
+            child: widget.appBar,
+          ),
+        ],
+        body: Builder(
+          builder: (bodyContext) {
+            _wrapTabViews(bodyContext);
+            return widget.tabBarView;
+          },
+        ),
+      ),
+    );
+  }
+
+  void _wrapTabViews(BuildContext context) {
+    final children = widget.tabBarView.children;
+    final wrappedChildren = children.map((it) => CustomScrollView(
+          slivers: <Widget>[
+            SliverOverlapInjector(
+              handle: NestedScrollView.sliverOverlapAbsorberHandleFor(context),
+            ),
+            SliverToBoxAdapter(child: it),
+          ],
+        ));
+    children.setRange(0, children.length, wrappedChildren);
+  }
+
+  bool _onScrollEvent(Notification notification) {
+    if (_isSnapping) return false;
+    if (notification is ScrollUpdateNotification) {
+      _lastScrollUpdate = notification;
+    } else if (notification is ScrollEndNotification) {
+      if (widget.scrollController.offset > 0.0 &&
+          widget.scrollController.offset < widget.collapsedOffset) {
+        _isSnapping = true;
+        _scheduleSnapScroll(_lastScrollUpdate.scrollDelta > 0.0);
+        return true;
+      }
+    }
+    return false;
+  }
+
+  void _scheduleSnapScroll(bool scrollsUpwards) {
+    Future(() async {
+      await widget.scroller(scrollsUpwards ? widget.collapsedOffset : 0.0);
+      _lastScrollUpdate = null;
+      _isSnapping = false;
+    });
+  }
+}
