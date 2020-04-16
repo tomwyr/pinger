d052cd8c2b5ccebb9684e78bff6a6a7beeb26612
tomwyr <tomwyr@hotmail.com>
2020-04-16 19:19:39 +0200

    Implement PingerApi calls

--- lib/service/pinger_api.dart
+++ lib/service/pinger_api.dart
@@ -1,14 +1,31 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:injectable/injectable.dart';
+import 'package:pinger/model/ping_global.dart';
 
 @injectable
 class PingerApi {
   final String _allPath = 'all';
   final String _countsPath = 'counts-monthly';
   final String _resultsPath = 'results-monthly';
-  final String _sessionsPath = 'session';
+  final String _sessionsPath = 'sessions';
 
   final Firestore _firestore;
 
   PingerApi(this._firestore);
+
+  Future<GlobalPingCounts> getPingCounts() async {
+    final countsSnap =
+        await _firestore.collection(_countsPath).document(_allPath).get();
+    return GlobalPingCounts.fromJson(countsSnap.data);
+  }
+
+  Future<GlobalHostResults> getHostResults(String host) async {
+    final resultsSnap =
+        await _firestore.collection(_resultsPath).document(host).get();
+    return GlobalHostResults.fromJson(resultsSnap.data);
+  }
+
+  Future<void> saveSessionResult(GlobalSessionResult result) async {
+    await _firestore.collection(_sessionsPath).add(result.toJson());
+  }
 }
