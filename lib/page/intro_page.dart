7283fc47f03b4318a02c714103446e21f14f353e
tomwyr <tomwyr@hotmail.com>
2020-06-06 01:26:49 +0200

    Improve intro page

--- lib/page/intro_page.dart
+++ lib/page/intro_page.dart
@@ -9,164 +9,43 @@ class IntroPage extends StatefulWidget {
 }
 
 class _IntroPageState extends State<IntroPage> {
-  final int _itemCount = 4;
+  final _indicatorColor = ColorTween(
+    begin: R.colors.secondary,
+    end: R.colors.grayLight,
+  );
+  final _bottomContentHeight = 160.0;
+  final _nextButtonSize = 64.0;
+  final _pageProgress = ValueNotifier(0.0);
+  final _itemCount = 4;
   int _currentItem = 0;
+  double _pageWidth;
   PageController _pageController;
 
   @override
   void initState() {
     super.initState();
-    _pageController = PageController(initialPage: _currentItem);
+    _pageController = PageController(initialPage: _currentItem)
+      ..addListener(_updateProgress);
   }
 
   @override
   void dispose() {
     _pageController.dispose();
+    _pageProgress.dispose();
     super.dispose();
   }
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(32.0),
-          child: Column(
-            children: <Widget>[
-              Spacer(),
-              _buildItemsPager(),
-              Spacer(),
-              _buildItemsIndicator(),
-              Spacer(),
-              _buildButtons(),
-            ],
-          ),
-        ),
-      ),
-    );
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    _pageWidth = MediaQuery.of(context).size.width;
+    _updateProgress();
   }
 
-  Widget _buildItemsPager() {
-    return Container(
-      height: 360.0,
-      child: PageView(
-        controller: _pageController,
-        onPageChanged: (it) => setState(() => _currentItem = it),
-        children: <Widget>[
-          _buildItem(
-            Images.undrawSearching,
-            "Select host",
-            "Either pick one of the search results or enter your own host and confirm your choice",
-          ),
-          _buildItem(
-            Images.undrawSettings,
-            "Adjust settings",
-            "Change host monitoring preferences and behavior of entire application",
-          ),
-          _buildItem(
-            Images.undrawSignalSearching,
-            "Ping host",
-            "Either perform a quick ping by long pressing play button or tap it to start ping session",
-          ),
-          _buildItem(
-            Images.undrawCollecting,
-            "Save results",
-            "Archive ping results to review them later and compare them with rest of the world",
-          ),
-        ],
-      ),
-    );
-  }
-
-  Widget _buildItem(AssetImage image, String title, String description) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 24.0),
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: <Widget>[
-          Image(image: image, width: 200.0, height: 200.0),
-          Container(height: 40.0),
-          Text(
-            title,
-            style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
-          ),
-          Container(height: 16.0),
-          Text(
-            description,
-            style: TextStyle(fontSize: 18.0),
-            textAlign: TextAlign.center,
-          ),
-        ],
-      ),
-    );
-  }
-
-  Widget _buildItemsIndicator() {
-    return Row(
-      mainAxisSize: MainAxisSize.min,
-      children: List<Widget>.generate(
-        _itemCount,
-        (it) => GestureDetector(
-          onTap: () => _moveToPage(it),
-          child: Container(
-            margin: const EdgeInsets.all(8.0),
-            height: 16.0,
-            width: it == _currentItem ? 32.0 : 16.0,
-            decoration: BoxDecoration(
-              color:
-                  it == _currentItem ? R.colors.secondary : R.colors.grayLight,
-              borderRadius: BorderRadius.circular(8.0),
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-
-  Widget _buildButtons() {
-    return SizedBox(
-      height: 64.0,
-      child: Align(
-        alignment: Alignment.center,
-        child: (_currentItem < _itemCount - 1)
-            ? Row(children: <Widget>[
-                ButtonTheme.fromButtonThemeData(
-                  data: R.themes.flatButton,
-                  child: FlatButton(child: Text("SKIP"), onPressed: pop),
-                ),
-                Spacer(),
-                SizedBox.fromSize(
-                  size: Size.square(64.0),
-                  child: RaisedButton(
-                    padding: EdgeInsets.zero,
-                    onPressed: _onNextPressed,
-                    child: Icon(Icons.arrow_forward, size: 32.0),
-                  ),
-                ),
-              ])
-            : IntrinsicWidth(
-                child: ButtonTheme.fromButtonThemeData(
-                  data: R.themes.raisedButton,
-                  child: RaisedButton(
-                    onPressed: pop,
-                    child: Row(children: <Widget>[
-                      Expanded(
-                        child: Text(
-                          "Get started",
-                          textAlign: TextAlign.center,
-                        ),
-                      ),
-                      SizedBox.fromSize(
-                        size: Size.square(32.0),
-                        child: Icon(Icons.arrow_forward, size: 24.0),
-                      ),
-                    ]),
-                  ),
-                ),
-              ),
-      ),
-    );
+  void _updateProgress() {
+    if (_pageController.hasClients) {
+      _pageProgress.value = _pageController.offset / _pageWidth;
+    }
   }
 
   void _onNextPressed() => _moveToPage(_currentItem + 1);
@@ -176,6 +55,229 @@ class _IntroPageState extends State<IntroPage> {
       index,
       duration: Duration(milliseconds: 300),
       curve: Curves.easeOut,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Stack(children: <Widget>[
+          Positioned.fill(
+            child: _buildItemsPager(),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Padding(
+              padding: const EdgeInsets.all(24.0),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  _buildItemsIndicator(),
+                  SizedBox(height: 16.0),
+                  _buildButtons(),
+                ],
+              ),
+            ),
+          ),
+        ]),
+      ),
+    );
+  }
+
+  Widget _buildItemsPager() {
+    return PageView(
+      controller: _pageController,
+      onPageChanged: (it) => _currentItem = it,
+      children: <Widget>[
+        _buildItem(
+          0,
+          Images.undrawSearching,
+          "Select host",
+          "Either pick one of the search results or enter your own host and confirm your choice",
+        ),
+        _buildItem(
+          1,
+          Images.undrawSettings,
+          "Adjust settings",
+          "Change host monitoring preferences and behavior of entire application",
+        ),
+        _buildItem(
+          2,
+          Images.undrawSignalSearching,
+          "Ping host",
+          "Either perform a quick ping by long pressing play button or tap it to start ping session",
+        ),
+        _buildItem(
+          3,
+          Images.undrawCollecting,
+          "Save results",
+          "Archive ping results to review them later and compare them with rest of the world",
+        ),
+      ],
+    );
+  }
+
+  Widget _buildItem(
+      int index, AssetImage image, String title, String description) {
+    return Align(
+      alignment: Alignment.topCenter,
+      child: Container(
+        margin: EdgeInsets.only(bottom: _bottomContentHeight),
+        padding: const EdgeInsets.fromLTRB(32.0, 0.0, 32.0, 16.0),
+        alignment: Alignment.center,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Spacer(),
+            _buildItemImage(index, image),
+            Spacer(),
+            Text(
+              title,
+              style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
+            ),
+            Container(height: 16.0),
+            Text(
+              description,
+              style: TextStyle(fontSize: 18.0),
+              textAlign: TextAlign.center,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildItemImage(int index, AssetImage image) {
+    return ValueListenableBuilder<double>(
+      valueListenable: _pageProgress,
+      builder: (_, progress, child) {
+        final relativeProgress = (progress - index).clamp(-1.0, 1.0).abs();
+        return Opacity(
+          opacity: 1 - relativeProgress,
+          child: FractionalTranslation(
+            translation: Offset(relativeProgress, 0.0),
+            child: child,
+          ),
+        );
+      },
+      child: Image(image: image, width: 200.0, height: 200.0),
+    );
+  }
+
+  Widget _buildItemsIndicator() {
+    return ValueListenableBuilder<double>(
+      valueListenable: _pageProgress,
+      builder: (_, progress, __) => Row(
+        mainAxisSize: MainAxisSize.min,
+        children: List<Widget>.generate(
+          _itemCount,
+          (index) {
+            final relativeProgress = (progress - index).clamp(-1.0, 1.0).abs();
+            return GestureDetector(
+              onTap: () => _moveToPage(index),
+              child: Container(
+                margin: const EdgeInsets.all(8.0),
+                height: 16.0,
+                width: 32.0 - 16.0 * relativeProgress,
+                decoration: BoxDecoration(
+                  color: _indicatorColor.transform(relativeProgress),
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+
+  Widget _buildButtons() {
+    return ValueListenableBuilder<double>(
+      valueListenable: _pageProgress,
+      builder: (_, progress, __) => SizedBox(
+        height: _nextButtonSize,
+        child: Align(
+          alignment: Alignment.center,
+          child: TweenAnimationBuilder<double>(
+            duration: const Duration(milliseconds: 300),
+            curve: Curves.easeInOut,
+            tween: Tween(
+              begin: 0.0,
+              end: progress > _itemCount - 1.5 ? 1.0 : 0.0,
+            ),
+            builder: (_, expansion, __) => SizedBox(
+              height: _nextButtonSize,
+              child: Stack(children: <Widget>[
+                _buildSkipButton(expansion),
+                _buildNextButton(expansion),
+              ]),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildSkipButton(double value) {
+    return Align(
+      alignment: Alignment.centerLeft,
+      child: Opacity(
+        opacity: 1.0 - value,
+        child: ButtonTheme.fromButtonThemeData(
+          data: R.themes.flatButton,
+          child: FlatButton(
+            child: Text("SKIP"),
+            onPressed: value < 1.0 ? pop : null,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildNextButton(double expansion) {
+    final raisedButtonWidth =
+        (R.themes.raisedButton.minWidth - _nextButtonSize) * expansion +
+            _nextButtonSize;
+    return Align(
+      alignment: Alignment(1.0 - expansion, 0.0),
+      child: SizedBox(
+        height: _nextButtonSize - 16.0 * expansion,
+        child: ButtonTheme.fromButtonThemeData(
+          data: R.themes.raisedButton.copyWith(
+            minWidth: raisedButtonWidth,
+          ),
+          child: RaisedButton(
+            padding: EdgeInsets.only(
+              left: 20.0 * expansion,
+              right: 12.0 * expansion,
+            ),
+            onPressed: expansion > 0.5 ? pop : _onNextPressed,
+            child: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                ClipRect(
+                  child: Align(
+                    widthFactor: expansion,
+                    child: Padding(
+                      padding: const EdgeInsets.only(right: 12.0),
+                      child: Opacity(
+                        opacity: expansion,
+                        child: Text("Get started"),
+                      ),
+                    ),
+                  ),
+                ),
+                Icon(
+                  Icons.arrow_forward,
+                  size: _nextButtonSize / 2 - 8.0 * expansion,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
