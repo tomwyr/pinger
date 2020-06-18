7d7e2e034997b5c6a74022efa181022b353c388c
tomwyr <tomwyr@hotmail.com>
2020-06-18 17:35:59 +0200

    Move images to a separate directory

--- lib/assets.dart
+++ lib/assets.dart
@@ -1,29 +1,29 @@
 import 'package:flutter/widgets.dart';
 
 class Images {
-  /// ![](file:///C:/dev/projects/pinger/lib/assets/splash-hd.png)
-  static AssetImage get splashHd => const AssetImage("lib/assets/splash-hd.png");
-  /// ![](file:///C:/dev/projects/pinger/lib/assets/splash.png)
-  static AssetImage get splash => const AssetImage("lib/assets/splash.png");
-  /// ![](file:///C:/dev/projects/pinger/lib/assets/undraw_collecting.png)
-  static AssetImage get undrawCollecting => const AssetImage("lib/assets/undraw_collecting.png");
-  /// ![](file:///C:/dev/projects/pinger/lib/assets/undraw_empty.png)
-  static AssetImage get undrawEmpty => const AssetImage("lib/assets/undraw_empty.png");
-  /// ![](file:///C:/dev/projects/pinger/lib/assets/undraw_road_sign.png)
-  static AssetImage get undrawRoadSign => const AssetImage("lib/assets/undraw_road_sign.png");
-  /// ![](file:///C:/dev/projects/pinger/lib/assets/undraw_runner_start.png)
-  static AssetImage get undrawRunnerStart => const AssetImage("lib/assets/undraw_runner_start.png");
-  /// ![](file:///C:/dev/projects/pinger/lib/assets/undraw_searching.png)
-  static AssetImage get undrawSearching => const AssetImage("lib/assets/undraw_searching.png");
-  /// ![](file:///C:/dev/projects/pinger/lib/assets/undraw_server_down.png)
-  static AssetImage get undrawServerDown => const AssetImage("lib/assets/undraw_server_down.png");
-  /// ![](file:///C:/dev/projects/pinger/lib/assets/undraw_settings.png)
-  static AssetImage get undrawSettings => const AssetImage("lib/assets/undraw_settings.png");
-  /// ![](file:///C:/dev/projects/pinger/lib/assets/undraw_signal_searching.png)
-  static AssetImage get undrawSignalSearching => const AssetImage("lib/assets/undraw_signal_searching.png");
-  /// ![](file:///C:/dev/projects/pinger/lib/assets/undraw_the_world_is_mine.png)
-  static AssetImage get undrawTheWorldIsMine => const AssetImage("lib/assets/undraw_the_world_is_mine.png");
-  /// ![](file:///C:/dev/projects/pinger/lib/assets/undraw_void.png)
-  static AssetImage get undrawVoid => const AssetImage("lib/assets/undraw_void.png");
+  /// ![](file:///C:/dev/projects/pinger/lib/assets/images/splash-hd.png)
+  static AssetImage get splashHd => const AssetImage("lib/assets/images/splash-hd.png");
+  /// ![](file:///C:/dev/projects/pinger/lib/assets/images/splash.png)
+  static AssetImage get splash => const AssetImage("lib/assets/images/splash.png");
+  /// ![](file:///C:/dev/projects/pinger/lib/assets/images/undraw_collecting.png)
+  static AssetImage get undrawCollecting => const AssetImage("lib/assets/images/undraw_collecting.png");
+  /// ![](file:///C:/dev/projects/pinger/lib/assets/images/undraw_empty.png)
+  static AssetImage get undrawEmpty => const AssetImage("lib/assets/images/undraw_empty.png");
+  /// ![](file:///C:/dev/projects/pinger/lib/assets/images/undraw_road_sign.png)
+  static AssetImage get undrawRoadSign => const AssetImage("lib/assets/images/undraw_road_sign.png");
+  /// ![](file:///C:/dev/projects/pinger/lib/assets/images/undraw_runner_start.png)
+  static AssetImage get undrawRunnerStart => const AssetImage("lib/assets/images/undraw_runner_start.png");
+  /// ![](file:///C:/dev/projects/pinger/lib/assets/images/undraw_searching.png)
+  static AssetImage get undrawSearching => const AssetImage("lib/assets/images/undraw_searching.png");
+  /// ![](file:///C:/dev/projects/pinger/lib/assets/images/undraw_server_down.png)
+  static AssetImage get undrawServerDown => const AssetImage("lib/assets/images/undraw_server_down.png");
+  /// ![](file:///C:/dev/projects/pinger/lib/assets/images/undraw_settings.png)
+  static AssetImage get undrawSettings => const AssetImage("lib/assets/images/undraw_settings.png");
+  /// ![](file:///C:/dev/projects/pinger/lib/assets/images/undraw_signal_searching.png)
+  static AssetImage get undrawSignalSearching => const AssetImage("lib/assets/images/undraw_signal_searching.png");
+  /// ![](file:///C:/dev/projects/pinger/lib/assets/images/undraw_the_world_is_mine.png)
+  static AssetImage get undrawTheWorldIsMine => const AssetImage("lib/assets/images/undraw_the_world_is_mine.png");
+  /// ![](file:///C:/dev/projects/pinger/lib/assets/images/undraw_void.png)
+  static AssetImage get undrawVoid => const AssetImage("lib/assets/images/undraw_void.png");
 }
 
