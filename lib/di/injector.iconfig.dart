a8157d368f2c37ca1d1456a887237b251c986434
tomwyr <tomwyr@hotmail.com>
2020-07-13 18:30:46 +0200

    Create LifecycleNotifier class

--- lib/di/injector.iconfig.dart
+++ lib/di/injector.iconfig.dart
@@ -9,6 +9,7 @@ import 'package:connectivity/connectivity.dart';
 import 'package:pinger/service/favicon_service.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_local_notifications/src/flutter_local_notifications_plugin.dart';
+import 'package:pinger/utils/lifecycle_notifier.dart';
 import 'package:location/location.dart';
 import 'package:pinger/utils/notification_messages.dart';
 import 'package:package_info/package_info.dart';
@@ -30,6 +31,7 @@ Future<void> $initGetIt(GetIt g, {String environment}) async {
   g.registerFactory<Firestore>(() => injectorModule.firestore);
   g.registerFactory<FlutterLocalNotificationsPlugin>(
       () => injectorModule.localNotifications);
+  g.registerFactory<LifecycleNotifier>(() => LifecycleNotifier());
   g.registerFactory<Location>(() => injectorModule.location);
   g.registerFactory<NotificationMessages>(
       () => injectorModule.notificationLocalizations);
@@ -66,6 +68,7 @@ Future<void> $initGetIt(GetIt g, {String environment}) async {
     g<LocationStore>(),
   ));
   g.registerSingleton<NotificationStore>(NotificationStore(
+    g<LifecycleNotifier>(),
     g<FlutterLocalNotificationsPlugin>(),
     g<NotificationMessages>(),
     g<SettingsStore>(),
