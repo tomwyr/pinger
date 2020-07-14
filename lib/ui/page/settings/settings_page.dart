d59edd1e5bcedbabdce3eea3f76544194736cdcf
tomwyr <tomwyr@hotmail.com>
2020-07-14 11:29:38 +0200

    Show missing notification permission sheet

--- lib/ui/page/settings/settings_page.dart
+++ lib/ui/page/settings/settings_page.dart
@@ -1,22 +1,28 @@
+import 'package:app_settings/app_settings.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:mobx/mobx.dart';
 import 'package:pinger/di/injector.dart';
 import 'package:pinger/extensions.dart';
 import 'package:pinger/generated/l10n.dart';
 import 'package:pinger/resources.dart';
+import 'package:pinger/store/notification_store.dart';
 import 'package:pinger/store/settings_store.dart';
 import 'package:pinger/ui/common/scroll_edge_gradient.dart';
 import 'package:pinger/ui/page/base_page.dart';
 import 'package:pinger/ui/page/intro_page.dart';
 import 'package:pinger/ui/page/settings/settings_sections.dart';
+import 'package:pinger/ui/shared/sheet/pinger_bottom_sheet.dart';
 
 class SettingsPage extends StatefulWidget {
   @override
   _SettingsPageState createState() => _SettingsPageState();
 }
 
-class _SettingsPageState extends BaseState<SettingsPage> {
+class _SettingsPageState extends BaseState<SettingsPage>
+    with NotificationPermissionState {
   final SettingsStore _settingsStore = Injector.resolve();
+  final NotificationStore _notificationStore = Injector.resolve();
 
   @override
   Widget build(BuildContext context) {
@@ -64,5 +70,65 @@ class _SettingsPageState extends BaseState<SettingsPage> {
         );
       }),
     );
+  }
+}
+
+mixin NotificationPermissionState<T extends StatefulWidget> on State<T> {
+  SettingsStore get _settingsStore;
+  NotificationStore get _notificationStore;
+
+  bool _isShowingSheet = false;
+  ReactionDisposer _disposer;
+
+  @override
+  void initState() {
+    super.initState();
+    _disposer = autorun((_) {
+      final hasPermission = _notificationStore.hasPermission;
+      final showNotification =
+          _settingsStore.userSettings.showSystemNotification;
+      if (!showNotification && hasPermission == true && _isShowingSheet) {
+        pop();
+        _settingsStore.updateSettings(
+          _settingsStore.userSettings.copyWith(
+            showSystemNotification: true,
+          ),
+        );
+      } else if (showNotification &&
+          hasPermission == false &&
+          !_isShowingSheet) {
+        _settingsStore.updateSettings(
+          _settingsStore.userSettings.copyWith(
+            showSystemNotification: false,
+          ),
+        );
+        _showMissingPermissionSheet();
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _disposer();
+    super.dispose();
+  }
+
+  void _showMissingPermissionSheet() async {
+    _isShowingSheet = true;
+    await Future(() => PingerBottomSheet.show(
+          context,
+          title: Text(
+            "Notification permission disabled",
+            style: R.styles.bottomSheetTitle,
+          ),
+          subtitle: Text(
+            "Enable permission in app settings",
+            style: R.styles.bottomSheetSubtitle,
+          ),
+          acceptIcon: Icons.settings,
+          rejectLabel: S.current.cancelButtonLabel,
+          onAcceptPressed: AppSettings.openNotificationSettings,
+        ));
+    _isShowingSheet = false;
   }
 }
--- lib/ui/shared/sheet/pinger_bottom_sheet.dart
+++ lib/ui/shared/sheet/pinger_bottom_sheet.dart
@@ -8,6 +8,7 @@ class PingerBottomSheet extends StatelessWidget {
     BuildContext context, {
     Widget title,
     Widget subtitle,
+    IconData acceptIcon = Icons.check,
     String rejectLabel,
     VoidCallback onRejectPressed,
     VoidCallback onAcceptPressed,
@@ -24,6 +25,7 @@ class PingerBottomSheet extends StatelessWidget {
       transitionBuilder: (_, transition, __, child) => PingerBottomSheet._(
         title: title,
         subtitle: subtitle,
+        acceptIcon: acceptIcon,
         rejectLabel: rejectLabel,
         onRejectPressed: onRejectPressed,
         onAcceptPressed: onAcceptPressed,
@@ -38,6 +40,7 @@ class PingerBottomSheet extends StatelessWidget {
     Key key,
     @required this.title,
     @required this.subtitle,
+    @required this.acceptIcon,
     @required this.rejectLabel,
     @required this.onRejectPressed,
     @required this.onAcceptPressed,
@@ -51,6 +54,7 @@ class PingerBottomSheet extends StatelessWidget {
   final Animation<double> transition;
   final Widget title;
   final Widget subtitle;
+  final IconData acceptIcon;
   final String rejectLabel;
   final VoidCallback onRejectPressed;
   final VoidCallback onAcceptPressed;
@@ -149,7 +153,7 @@ class PingerBottomSheet extends StatelessWidget {
                   onPressed: canAccept == null || canAccept()
                       ? onAcceptPressed ?? Navigator.of(context).pop
                       : null,
-                  child: Icon(Icons.check),
+                  child: Icon(acceptIcon),
                 ),
               ),
             ],
