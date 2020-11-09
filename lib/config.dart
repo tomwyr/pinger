5ab706c482502abb41a6616ec9ac38689f2ad17b
tomwyr <tomwyr@hotmail.com>
2020-11-09 12:13:32 +0100

    Remove tst flavor

--- lib/config.dart
+++ lib/config.dart
@@ -5,13 +5,6 @@ abstract class AppConfig {
   String get iconPath;
 }
 
-@test
-@Injectable(as: AppConfig)
-class TestConfig extends AppConfig {
-  @override
-  String get iconPath => Images.appIconTst.assetName;
-}
-
 @dev
 @Injectable(as: AppConfig)
 class DevConfig extends AppConfig {
